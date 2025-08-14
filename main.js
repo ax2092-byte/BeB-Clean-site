1814aa44a0ea254bd69f2739673ba3befb2560e3
ax2092-byte <ax2092@gmail.com>
2025-08-14 10:46:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
 
-document.addEventListener('submit', function(e){
-  const btn = e.target.querySelector('button[type="submit"]');
-  if (btn){ btn.disabled = true; btn.textContent = 'Invio…'; }
-});
